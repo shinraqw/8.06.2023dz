d6bf79730eacd21a47c4aa9afa291c59589e52eb
shinraqw <oliynik05vitaliy@gmail.com>
2023-06-19 19:05:08 +0300

    Add project files.

---
+++ 8.06.2023dz/Class1.cs
@@ -0,0 +1,74 @@
+﻿using _8._06._2023dz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._06._2023dz
+{
+    public class Device
+    {
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public decimal Price { get; set; }
+
+
+        public static List<Device> CreateDevicesList1()
+        {
+            return new List<Device>
+        {
+            new Device { Name = "Смартфон", Manufacturer = "Samsung", Price = 5000 },
+            new Device { Name = "Ноутбук", Manufacturer = "Apple", Price = 20000 },
+            new Device { Name = "Телевізор", Manufacturer = "LG", Price = 15000 }
+        };
+        }
+
+        public static List<Device> CreateDevicesList2()
+        {
+            return new List<Device>
+        {
+            new Device { Name = "Смартфон", Manufacturer = "Samsung", Price = 900 },
+            new Device { Name = "Телевізор", Manufacturer = "Sony", Price = 12000 },
+            new Device { Name = "Монітор", Manufacturer = "Dell", Price = 500 }
+        };
+        }
+
+        public static List<Device> GetDifference(List<Device> devices1, List<Device> devices2)
+        {
+            return devices1.Where(d1 => !devices2.Any(d2 => d1.Manufacturer == d2.Manufacturer)).ToList();
+        }
+
+        public static List<Device> GetIntersection(List<Device> devices1, List<Device> devices2)
+        {
+            return devices1.Where(d1 => devices2.Any(d2 => d1.Manufacturer == d2.Manufacturer)).ToList();
+        }
+
+        public static List<Device> GetUnion(List<Device> devices1, List<Device> devices2)
+        {
+            return devices1.Union(devices2, new DeviceComparer()).ToList();
+        }
+
+        public static void PrintDevices(List<Device> devices)
+        {
+            foreach (var device in devices)
+            {
+                Console.WriteLine($"Назва: {device.Name}, Виробник: {device.Manufacturer}, Вартість: {device.Price}");
+            }
+        }
+    }
+}
+public class DeviceComparer : IEqualityComparer<Device>
+{
+    public bool Equals(Device x, Device y)
+    {
+        return x.Manufacturer == y.Manufacturer;
+    }
+
+    public int GetHashCode(Device obj)
+    {
+        return obj.Manufacturer.GetHashCode();
+    }
+
+
+}
---
+++ 8.06.2023dz/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace _8._06._2023dz
+{
+
+    public class CustomNumberComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            int sumX = GetDigitSum(x);
+            int sumY = GetDigitSum(y);
+
+            return sumX.CompareTo(sumY);
+        }
+
+        private int GetDigitSum(int number)
+        {
+            int sum = 0;
+            while (number != 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main()
+        {
+            //1
+            int[] numbers = { 121, 75, 81 };
+            Array.Sort(numbers, new CustomNumberComparer());
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            //2
+            string[] countries1 = { "Україна", "Польща", "США", "Німеччина", "Франція" };
+            string[] countries2 = { "Франція", "Німеччина", "Італія", "Китай", "Україна" };
+
+            // різниця
+            var difference = countries1.Except(countries2);
+            Console.WriteLine("Різниця між масивами:");
+            foreach (string country in difference)
+            {
+                Console.WriteLine(country);
+            }
+
+            // перетин
+            var intersection = countries1.Intersect(countries2);
+            Console.WriteLine("\nПеретин масивів:");
+            foreach (string country in intersection)
+            {
+                Console.WriteLine(country);
+            }
+
+            // об'єднання
+            var union = countries1.Union(countries2);
+            Console.WriteLine("\nОб'єднання масивів:");
+            foreach (string country in union)
+            {
+                Console.WriteLine(country);
+            }
+
+            // вміст першого масиву без повторень
+            var distinct = countries1.Distinct();
+            Console.WriteLine("\nВміст першого масиву без повторень:");
+            foreach (string country in distinct)
+            {
+                Console.WriteLine(country);
+            }
+
+            //3
+
+            List<Device> devices1 = Device.CreateDevicesList1();
+            List<Device> devices2 = Device.CreateDevicesList2();
+
+            List<Device> difference3 = Device.GetDifference(devices1, devices2);
+            Console.WriteLine("Різниця масивів за виробником:");
+            Device.PrintDevices(difference3);
+
+            List<Device> intersection3 = Device.GetIntersection(devices1, devices2);
+            Console.WriteLine("\nПеретин масивів за виробником:");
+            Device.PrintDevices(intersection3);
+
+            List<Device> union3 = Device.GetUnion(devices1, devices2);
+            Console.WriteLine("\nОб'єднання масивів за виробником:");
+            Device.PrintDevices(union3);
+        }
+    }
+}
